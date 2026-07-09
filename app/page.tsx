0a100de3717f0c699017f38282a6be6175dce122
gonjj <gonzales.joshua.james@gmail.com>
2026-07-09 21:12:20 +0800

    supabase integration

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,16 @@ import Hero from "../components/Hero";
 import Countdown from "../components/Countdown";
 import Venue from "../components/Venue";
 import Invitation from "../components/Invitation";
+import RSVP from "../components/RSVP";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-     <Invitation/>
+      <Invitation />
+      <RSVP />
       <Countdown />
-      <Venue/>
-      
+      <Venue />
     </main>
   );
 }
